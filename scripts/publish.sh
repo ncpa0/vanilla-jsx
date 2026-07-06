a3ed6ade2ae911068cc32d6dffaa76c3c5b3f664
ncpa0cpl <szymonb21@gmail.com>
2026-07-06 17:43:18 +0200

    chore: npm publish specify the tag as latest

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,4 +7,4 @@ git config user.name github-actions
 git config user.email github-actions@github.com
 
 echo "Publishing to npm"
-npm publish --access public
+npm publish --access public --tag latest
